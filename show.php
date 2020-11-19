bd67478ef4ec60e9a2a920386a0cdb632be5b374
it63070230 <73694109+it63070230@users.noreply.github.com>
2020-11-19 17:01:40 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,55 +1,54 @@
 <html>
 <head>
-<title>ITF Lab</title>
-  <meta charset="utf-8">
+<title>ITF Lab Show information</title>
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-  p.ex1 {
-    margin-top: 25px;
-  }
-  </style>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<style>
+body {
+  background-color: rgba(172, 207, 246, 1);
+}
+</style>
 </head>
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'peak-555.mysql.database.azure.com', 'SilverShade@peak-555', 'Peak0307', 'itflab', 3306);
+mysqli_real_connect($conn, 'tanadon.mysql.database.azure.com', 'tanadonparosin@tanadon', 'Nexus404', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-
-<div class="container">
-    <div class="form">
-        <p class="ex1"><h1><center>ITF Show</center></h1></p>
-    </div>
-  <table width="600" border="1" class="table table-hover" class="center" class="ex1">
-    <tr class="info">
-      <th width="100"> <div align="center">Name</div></th>
-      <th width="350"> <div align="center">Comment </div></th>
-      <th width="150"> <div align="center">Action</div></th>
-    </tr>
-  <?php
-  while($Result = mysqli_fetch_array($res))
-  {
-  ?>
-    <tr>
-      <td><center><?php echo $Result['name'];?></center></td>
-      <td><center><?php echo $Result['Comment'];?></center></td>
-      <td><center><a href="form_edit.html"><input type="submit" value="Edit" class="btn btn-warning"></a>&nbsp;&nbsp;<a href="form_delete.html"><input type="submit" value="Delete"  class="btn btn-danger"></a></center></td>
-    </tr>
-  <?php
-  }
-  ?>
-  </table>
-  <center><a href="form_insert.html"><input type="submit" value="Insert" class="btn btn-info"></a></center>
-  <?php
-  mysqli_close($conn);
-  ?>
-</div>
+<center>
+<table class="table table-hover table-bordered table-center" style="width:800px">
+  <tr class="table-primary" >
+    <th>Name</th>
+    <th>Comment</th>
+    <th>Link</th>
+  </tr>
+<center\>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr class="table-primary">
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+<a href="https://itflab2020.azurewebsites.net/form.html" class="btn btn-info">add</a>
+<a href="https://itflab2020.azurewebsites.net/form_edit.html" class="btn btn-warning">edit</a>
+<a href="https://itflab2020.azurewebsites.net/form_delete.html" class="btn btn-danger">delete</a>
 </body>
 </html>
